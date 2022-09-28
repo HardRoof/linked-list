e69d39001983c8d238e8a9114c21fa938c11ea6a
HardRoof <mateus_grandez@hotmail.com>
2022-09-28 17:04:52 -0600

    Added a few methods to LinkedList class

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(data = null, next = null) {
+  constructor(data, next = null) {
     this.data = data;
     this.next = next;
   }
@@ -10,17 +10,18 @@ class LinkedList {
     this.head = null; // Null by default. If list is empty there is no head
     this.size = 0; // Zero by default, to keep track of size
   }
-  // Adds first node
+
+  // Adds a new first node
   appendHead(data) {
-    this.head = new Node(data, this.head); // New Head.next is the current head
+    this.head = new Node(data, this.head); // New head's next is the current head
     this.size++;
   }
 
-  // Adds last node
+  // Adds a new last node
   appendTail(data) {
-    let tail = new Node(data); //
-    let current; // Initialize current in out scope
-    if (ll.size === 0) this.appendHead(data);
+    let tail = new Node(data);
+    let current; // Initialize 'current' in outer scope
+    if (this.size === 0) this.appendHead(data);
     else {
       current = this.head; // Starts from head and loop to find last node
       while (current.next !== null) {
@@ -33,8 +34,7 @@ class LinkedList {
 
   // inserts a new node with the provided value at the given index
   insertAt(data, index) {
-    // Out of range
-    if ((index > 0 && index > this.size) || index < 0) {
+    if (index < 0 || index > this.size) {
       return;
     } else if (index === 0) {
       this.appendHead(data);
@@ -54,18 +54,30 @@ class LinkedList {
     this.size++;
   }
 
-  // size - Total number of nodes
+  // Returns total number of nodes
+  printSize() {
+    console.log(this.size);
+  }
 
-  // head - Return head
+  // Returns head
+  headNode() {
+    console.log(this.head);
+  }
 
-  // tail - Return tail
+  // Returns tail
+  tailNode() {
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    console.log(current);
+  }
 
-  // at(index) - Returns node at given index
+  // Returns node at given index
   at(index) {
     let current = this.head;
     let currentPosition = 0;
     while (current) {
-      // null is falsy
       if (currentPosition === index) {
         console.log(current.data);
       }
@@ -75,41 +87,86 @@ class LinkedList {
     return null;
   }
 
-  // pop - Removes last node
-
-  // removeAt(index) - that removes the node at the given index
-  removeAt(index) {
-    if ((index > 0 && index > this.size) || index < 0) {
-      return;
+  // Returns true and index of the node containing value, or null if not found
+  contains(value, current = this.head, count = 0) {
+    if (current === null) return console.log(null);
+    if (value === current.data) {
+      return console.log(`true, index: ${count}`);
     }
+    this.contains(value, current.next, ++count);
   }
-
-  // contains(value) - Returns true if it contains value
-
-  // find(value) - Returns index of the node containing value, or null if not found
 
   // Prints your LinkedList objects as strings
   printList() {
-    let current = this.head; // It starts with head
-
-    while (current != null) {
+    let current = this.head;
+    while (current) {
       console.log(current.data.toString());
       current = current.next;
     }
   }
+
+  // Removes last node
+  pop() {
+    let current = this.head;
+    let previous;
+    if (this.size === 0) return;
+    else {
+      while (current.next !== null) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = null;
+    }
+    this.size--;
+  }
+
+  // Removes the node at the given index
+  removeAt(index) {
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    let current = this.head;
+    let previous;
+    let currentPosition = 0;
+    if (index === 0) {
+      this.head = current.next;
+      this.size--;
+      return;
+    }
+    while (currentPosition < index) {
+      previous = current;
+      current = current.next;
+      currentPosition++;
+    }
+    previous.next = current.next;
+    this.size--;
+  }
+
+  // Clears linked list
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
 }
 
-const ll = new LinkedList();
-console.log(ll);
+const myLinkedList = new LinkedList();
 
-ll.appendHead(300);
-ll.appendHead(200);
-ll.appendHead(100);
-ll.appendTail(400);
-ll.insertAt(50, 1);
-ll.insertAt(5000, 100);
-ll.insertAt(5000, -1);
+myLinkedList.appendHead(300); // Adds a head
+myLinkedList.appendHead(200); // Adds a head
+myLinkedList.appendHead(100); // Adds a head
+myLinkedList.appendTail(400); // Adds a tail
+myLinkedList.appendTail(500); // Adds a tail
+myLinkedList.insertAt(150, 1); // Inserts a node
+myLinkedList.insertAt(450, 5); // Inserts a node
 
-ll.printList();
-ll.at(1);
-ll.at(10);
+myLinkedList.printList(); // Returns 100 150 200 300 400 450 500
+myLinkedList.printSize(); // Returns 7
+
+myLinkedList.headNode(); // Returns Node {data: 100, next: Node}
+myLinkedList.tailNode(); // Returns Node {data: 500, next: null}
+
+myLinkedList.at(1); // Returns 150
+myLinkedList.at(10); // Returns nothing
+
+myLinkedList.contains(400); // Returns true, index: 4
+myLinkedList.contains(1000); // Returns null
